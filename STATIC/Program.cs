72229314c9d5fa0a5a75bef2f136bab2a6770000
Dell <sweta18singh@gmail.com>
2025-02-15 13:14:24 -0800

    Add project files.

---
+++ STATIC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+static class Utilites
+{
+    public static void Print(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+        Utilites.Print("Hello, World!");
+    }
+}
